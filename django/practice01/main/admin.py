ed42956d567e8981ce5a2ebf5c8f220f1e04b8bf
OngTK <dhdxorud@gmail.com>
2025-12-29 13:21:25 +0900

    django : 07. Django Admin (관리자 페이지)

--- django/practice01/main/admin.py
+++ django/practice01/main/admin.py
@@ -3,5 +3,13 @@ admin.py
 - 관리자 페이지 설정
 """
 from django.contrib import admin
+from .models import Post        # 7-4. model 등록
 
-# Register your models here.
+# 📌 7-4. Model을 Admin에 등록
+admin.site.register(Post)
+    
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at")
+    search_fields = ("title",)
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
