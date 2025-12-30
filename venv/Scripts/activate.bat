049f549286aa78a164b96a4d73465e5fcdb3a2dc
OngTK <dhdxorud@gmail.com>
2025-12-30 11:50:16 +0900

    build : venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\User\IdeaProjects\pythonStudy\venv"
+set "VIRTUAL_ENV=C:\Users\옹태경\IdeaProjects\pythonStudy\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
